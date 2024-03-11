cde511f4871ed96caf3f406c55f1955cbe110a3a
Decard <mail@dayreon.ru>
2024-03-11 15:32:55 +0800

    v1.0.0-beta12

--- remote/rsbuild.config.ts
+++ remote/rsbuild.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@rsbuild/core';
 import { rspack } from '@rspack/core';
 
 //==========================================
-import v100Beta11 from './mfes/v1.0.0-beta11';
+import v100Beta12 from './mfes/v1.0.0-beta12';
 //==========================================
 
 export default defineConfig({
@@ -32,7 +32,7 @@ export default defineConfig({
         react: 'React',
         'react-dom': 'ReactDOM'
       },
-      plugins: [new rspack.container.ModuleFederationPlugin(v100Beta11)],
+      plugins: [new rspack.container.ModuleFederationPlugin(v100Beta12)],
     },
   },
 });
