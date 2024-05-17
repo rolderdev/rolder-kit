97a5e2ce403dcb31f02970609b468366cc40a919
vezdexod <vezdexod@internet.ru>
2024-05-17 12:15:21 +0800

    Добавил timeout в ноду nodered

--- nodes/data/src/nodered/noderedNode.ts
+++ nodes/data/src/nodered/noderedNode.ts
@@ -47,6 +47,36 @@ export default jsNode(
 				getPort({ plug: 'output', name: 'executed', displayName: 'Executed', group: 'Signals', type: 'signal' }),
 				getPort({ plug: 'output', name: 'result', displayName: 'Result', group: 'Data', type: '*' })
 			]
+		},
+		'v1.1.0': { // Vezdexod
+			module: {
+				dynamic: import('@packages/nodered-v1.1.0')
+			},
+			inputs: [
+				getPort({ plug: 'input', name: 'execute', displayName: 'Execute', group: 'Signals', type: 'signal' }),
+				getPort({
+					plug: 'input',
+					name: 'flowEndpoint',
+					displayName: 'Flow endpoint',
+					group: 'Params',
+					type: 'string',
+					customs: { required: 'both' }
+				}),
+				getPort({ 							// Vezdexod
+					plug: 'input',
+					name: 'timeout',
+					displayName: 'timeout',
+					group: 'Params',
+					type: 'number',
+					default: 10000
+				}),
+				getPort({ plug: 'input', name: 'flowData', displayName: 'Flow data', group: 'Data', type: '*' })
+			],
+			outputs: [
+				getPort({ plug: 'output', name: 'executing', displayName: 'Executing', group: 'States', type: 'boolean', default: false }),
+				getPort({ plug: 'output', name: 'executed', displayName: 'Executed', group: 'Signals', type: 'signal' }),
+				getPort({ plug: 'output', name: 'result', displayName: 'Result', group: 'Data', type: '*' })
+			]
 		}
 	},
 	{ color: 'purple' }
