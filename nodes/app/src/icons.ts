489e0072cc551681bc124b377ec3f3dcb5f180c6
vezdexod <vezdexod@internet.ru>
2024-07-18 18:33:59 +0800

    IconSpeakerphone

--- nodes/app/src/icons.ts
+++ nodes/app/src/icons.ts
@@ -97,5 +97,6 @@ export {
 	IconLockOpen,			// Vezdexod
 	IconLinkPlus,			// Vezdexod 11.06
 	IconCopyPlus,			// Vezdexod 11.06
-	IconFilter				// Vezdexod 04.07
+	IconFilter,				// Vezdexod 04.07
+	IconSpeakerphone		// Vezdexod 17.07
 } from '@tabler/icons-react';
