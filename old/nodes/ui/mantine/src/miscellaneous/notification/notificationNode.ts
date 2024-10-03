ce801fa53f11bcc64f4d95418a934f19ad0ac9fc
Decard <mail@dayreon.ru>
2024-10-03 18:30:03 +0800

    v1.0.0-rc19

--- old/nodes/ui/mantine/src/miscellaneous/notification/notificationNode.ts
+++ old/nodes/ui/mantine/src/miscellaneous/notification/notificationNode.ts
@@ -2,7 +2,6 @@ import { jsNode } from '@packages/node';
 import { getPort, getPorts } from '@packages/port';
 
 import v100 from '@packages/notification-v1.0.0';
-import v200 from '@packages/notification-v2.0.0';
 
 export default jsNode(
 	'notification',
@@ -19,7 +18,7 @@ export default jsNode(
 					displayName: 'Message',
 					group: 'Params',
 					type: 'string',
-					customs: { required: 'connection' }
+					customs: { required: 'connection' },
 				}),
 				getPort({ plug: 'input', name: 'autoClose', displayName: 'Auto close', group: 'Params', type: 'boolean', default: true }),
 				getPort({
@@ -32,41 +31,11 @@ export default jsNode(
 					customs: {
 						dependsOn(p) {
 							return p.autoClose ? true : false;
-						}
-					}
-				})
-			]
+						},
+					},
+				}),
+			],
 		},
-		'v2.0.0': {
-			module: { static: v200 },
-			inputs: [
-				...getPorts('input', ['color']),
-				getPort({ plug: 'input', name: 'send', displayName: 'Send', group: 'Signals', type: 'signal' }),
-				getPort({ plug: 'input', name: 'title', displayName: 'Title', group: 'Params', type: 'string' }),
-				getPort({
-					plug: 'input',
-					name: 'message',
-					displayName: 'Message',
-					group: 'Params',
-					type: 'string',
-					customs: { required: 'connection' }
-				}),
-				getPort({ plug: 'input', name: 'autoClose', displayName: 'Auto close', group: 'Params', type: 'boolean', default: true }),
-				getPort({
-					plug: 'input',
-					name: 'autoCloseTimeout',
-					displayName: 'Auto close timeout',
-					group: 'Params',
-					type: 'number',
-					default: 2000,
-					customs: {
-						dependsOn(p) {
-							return p.autoClose ? true : false;
-						}
-					}
-				})
-			]
-		}
 	},
 	{ color: 'purple' }
 );
--- old/packages/components/ui/mantine/miscellaneous/notification/v2.0.0/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { BaseJsProps } from '@packages/node'
-
-export type Props = BaseJsProps & {
-    title?: string
-    message: string
-    color?: string
-    autoClose?: boolean
-    autoCloseTimeout?: number
-}
